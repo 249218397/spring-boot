24122f71ff3e4a2a9fd3a0e5567752ad04fea13f
赵强 <249218397@qq.com>
2020-01-15 11:30:03 +0800

    init rep

---
+++ src/main/java/com/zhaoq/doubuhui/doubuhui/DoubuhuiApplication.java
@@ -0,0 +1,13 @@
+package com.zhaoq.doubuhui.doubuhui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubuhuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubuhuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhaoq/doubuhui/doubuhui/DoubuhuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaoq.doubuhui.doubuhui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubuhuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
